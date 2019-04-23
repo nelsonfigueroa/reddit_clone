9e5e57a22eaf1600d49ba94ada5c62c09384db02
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-04-22 18:42:15 -0700

    added validations, added username to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # permit parameters for devise sign up page
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:username, :email, :password)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :trackable, :validatable
+
+  validates :email, :username, presence: true, uniqueness: true
+
 end

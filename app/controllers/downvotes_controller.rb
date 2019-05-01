ebc8a04d4e6a479001530c9f5e77f373aa39e08f
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-04-30 23:35:04 -0700

    completed voting functionality

--- app/controllers/downvotes_controller.rb
+++ app/controllers/downvotes_controller.rb
@@ -2,6 +2,13 @@ class DownvotesController < ApplicationController
 
   def create
     @downvote = Downvote.new(downvote_params)
+
+    # if user previously upvoted post, remove upvote
+    @upvote = Upvote.where(user_id: @downvote.user_id, post_id: @downvote.post_id).first
+    unless @upvote.nil?
+      @upvote.destroy
+    end
+
     @downvote.save
 
     # instead of redirecting, do AJAX in the future.
--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -2,6 +2,13 @@ class UpvotesController < ApplicationController
 
   def create
     @upvote = Upvote.new(upvote_params)
+
+    # if user previously downvoted post, remove downvote
+    @downvote = Downvote.where(user_id: @upvote.user_id, post_id: @upvote.post_id).first
+    unless @downvote.nil?
+      @downvote.destroy
+    end
+
     @upvote.save
 
     # instead of redirecting, do AJAX in the future.

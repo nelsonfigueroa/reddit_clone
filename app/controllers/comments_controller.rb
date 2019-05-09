c43d906a24d48b09e449223b2856b9a92824fc1b
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-05-08 21:26:18 -0700

    GOT AJAX WORKING

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,33 @@
 class CommentsController < ApplicationController
-  
-  # redirect_back(fallback_location: :back)
+
+  # def create
+  #   @comment = Comment.new(comment_params)
+
+  #   if @comment.save
+  #     redirect_back(fallback_location: :back)
+  #   else
+  #     flash[:notice] = "Invalid input!"
+  #     redirect_back(fallback_location: :back)
+  #   end
+  # end
 
   def create
     @comment = Comment.new(comment_params)
-
-    if @comment.save
-      redirect_back(fallback_location: :back)
-    else
-      flash[:notice] = "Invalid input!"
-      redirect_back(fallback_location: :back)
+   
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_back(fallback_location: :back, notice: 'Comment successfully created!') }
+        format.json { render json: @comment, status: :created, location: @comment }
+        format.js { redirect_back(fallback_location: :back, notice: 'Comment Successfully created!') }
+      else
+        format.html { redirect_back(fallback_location: :back, notice: 'Something went wrong') }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.js { redirect_back(fallback_location: :back, notice: 'Something went wrong') }
+      end
     end
   end
+
+  private
 
   def comment_params
     params.require(:comment).permit(:comment, :user_id, :post_id)

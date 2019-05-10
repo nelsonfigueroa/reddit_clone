61ac492b7987e4f345db01f1d1cda350bb240127
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-05-09 17:05:23 -0700

    added vote validations

--- app/models/downvote.rb
+++ app/models/downvote.rb
@@ -3,4 +3,7 @@ class Downvote < ApplicationRecord
   belongs_to :post
 
   validates :user_id, :post_id, presence: true
+
+  # user_id and post_id should be unique together
+  validates :user_id, uniqueness: { scope: :post_id }
 end

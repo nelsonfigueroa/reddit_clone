70edb879ab404cb1d34098a53f0b7135c4ffa47a
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-05-17 00:55:43 -0700

    implemented ajax for new votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,11 +4,16 @@ class VotesController < ApplicationController
     @vote = Vote.new(vote_params)
     @vote.user_id = current_user.id
 
-    if @vote.save
-      redirect_back(fallback_location: :back)
-    else
-      flash[:notice] = "Something went wrong..."
-      redirect_back(fallback_location: :back)
+    respond_to do |format|
+      if @vote.save
+        format.html { redirect_back(fallback_location: :back) }
+        format.json { render json: @vote, status: :created, location: @vote }
+        format.js
+      else
+        format.html { redirect_back(fallback_location: :back, notice: 'Something went wrong') }
+        format.json { render json: @vote.errors, status: :unprocessable_entity }
+        format.js
+      end
     end
 
   end

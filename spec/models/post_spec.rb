d6f2c57833bad153a9adda8291a9252cecf966a9
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-06-09 20:51:33 -0700

    added more specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+
+  # run before every example
+  before(:example) do
+    @post = Post.new
+  end
+
   it "validates that title and content are present" do
-    expect(Post.new.valid?).to be false
+    expect(@post.valid?).to be false
   end
 
   it "returns an integer when calling #net_votes" do
-    expect(Post.new.net_votes).to be_an(Integer) 
+    expect(@post.net_votes).to be_an(Integer) 
   end
 end
---
+++ spec/models/sub_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Sub, type: :model do
+
+  # run before every example
+  before(:example) do
+    @sub = Sub.new
+  end
+
+  it "validates that name and description are present" do
+    expect(@sub.valid?).to be false
+  end
+end

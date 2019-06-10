3e337baefdc44e2ef6fe241356f6073f12b4e3f4
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-06-09 21:10:17 -0700

    created spec for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  it "validates that email and username are present" do
+    expect(User.new.valid?).to be false
+  end
+
+  it "determines whether the user has voted on a post with #has_not_voted"
+    pending("Figuring out the best way to go about this.")
+  end
+
+  it "determines whether a user has upvoted a post with #upvoted_post?"
+    pending("as above")
+  end
+
+  it "determines whether a user has downvoted a post with #downvoted_post?"
+    pending("as above")
+  end
+
+  it "returns posts that were upvoted by user with #upvoted_posts"
+    pending("as above")
+  end
+
+  it "returns posts that were downvoted by a user with #downvoted_posts"
+    pending("as above")
+  end
+
+end

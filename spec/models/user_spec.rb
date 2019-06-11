5623b0ad4bf800dcd5ec3ff84c0c043519d084f6
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-06-10 20:31:50 -0700

    fixed syntax errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,23 +6,23 @@ RSpec.describe User, type: :model do
     expect(User.new.valid?).to be false
   end
 
-  it "determines whether the user has voted on a post with #has_not_voted"
+  it "determines whether the user has voted on a post with #has_not_voted" do
     pending("Figuring out the best way to go about this.")
   end
 
-  it "determines whether a user has upvoted a post with #upvoted_post?"
+  it "determines whether a user has upvoted a post with #upvoted_post?" do
     pending("as above")
   end
 
-  it "determines whether a user has downvoted a post with #downvoted_post?"
+  it "determines whether a user has downvoted a post with #downvoted_post?" do
     pending("as above")
   end
 
-  it "returns posts that were upvoted by user with #upvoted_posts"
+  it "returns posts that were upvoted by user with #upvoted_posts" do
     pending("as above")
   end
 
-  it "returns posts that were downvoted by a user with #downvoted_posts"
+  it "returns posts that were downvoted by a user with #downvoted_posts" do
     pending("as above")
   end
 

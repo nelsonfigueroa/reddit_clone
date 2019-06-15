6156aec33a80bd30d85b94024c0d3114c787490c
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-06-15 02:30:25 -0700

    customized rubocop, changed ruby version

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -3,7 +3,7 @@
 class Sub < ApplicationRecord
   # sub owner
   belongs_to :user
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true

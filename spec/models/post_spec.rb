50ce058610b39bd7fe8632df940e66584e8a686a
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-07-06 20:24:45 -0700

    completed sub specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 require 'pry'
 
 RSpec.describe Post, type: :model do
-  # run before every example
   let(:post) { Post.new }
   let(:user) { User.new }
   let(:sub) { Sub.new }
@@ -14,7 +13,7 @@ RSpec.describe Post, type: :model do
       expect(post.valid?).to be false
     end
   
-    it 'validates that title and content are present' do
+    it 'validates that all attributes are present' do
       post.title = 'Title'
       post.content = 'Content'
       post.user = user
--- spec/models/sub_spec.rb
+++ spec/models/sub_spec.rb
@@ -3,11 +3,34 @@
 require 'rails_helper'
 
 RSpec.describe Sub, type: :model do
-  before(:example) do
-    @sub = Sub.new
-  end
+  let(:post) { Post.new }
+  let(:user) { User.new }
+  let(:sub) { Sub.new }
 
-  it 'validates that name and description are present' do
-    expect(@sub.valid?).to be false
+  context 'creating a sub' do 
+    it 'returns invalid if attributes are missing' do
+      expect(sub.valid?).to be false
+    end
+
+    it 'validates that all attributes are present' do
+      sub.name = 'Sub Name'
+      sub.description = 'Sub Description'
+      sub.user = user
+
+      expect(sub.valid?).to be true
+    end
+
+    it 'validates that sub name is unique' do
+      sub.name = 'Sub Name'
+      sub.description = 'Sub Description'
+      sub.user = user
+      sub.save!
+
+      sub_two = sub.dup
+      sub_two.user = user
+      
+      # curly braces needed in this case
+      expect{sub_two.save!}.to raise_error(ActiveRecord::RecordInvalid)
+    end
   end
 end

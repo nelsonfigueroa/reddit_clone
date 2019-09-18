caff2c7a950468889c2e85898d22b928fdef6005
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-09-17 20:18:24 -0700

    completed user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -116,10 +116,98 @@ RSpec.describe User, type: :model do
   end
 
   it 'returns posts that were upvoted by user with #upvoted_posts' do
-    pending('as above')
+    user = User.create(
+      email: 'test@test.com',
+      password: 'testing123',
+      username: 'test'
+    )
+
+    sub = Sub.create(
+      user: user,
+      name: 'sub name',
+      description: 'sub description'
+    )
+
+    post = Post.create(
+      title: 'post title',
+      content: 'post content',
+      user: user,
+      sub: sub
+    )
+
+    vote = Vote.create(
+      user_id: user.id,
+      post_id: post.id,
+      upvote: 1,
+      downvote: 0
+    )
+
+    post_two = Post.create(
+      title: 'post title',
+      content: 'post content',
+      user: user,
+      sub: sub
+    )
+
+    # downvoted post this time
+    vote_two = Vote.create(
+      user_id: user.id,
+      post_id: post_two.id,
+      upvote: 0,
+      downvote: 1
+    )
+
+    user_upvoted_posts = user.upvoted_posts
+    all_upvoted_posts = Post.joins(:votes).where(:votes => { upvote: 1, downvote: 0} )
+
+    expect(user_upvoted_posts).to match_array(all_upvoted_posts)
   end
 
   it 'returns posts that were downvoted by a user with #downvoted_posts' do
-    pending('as above')
+    user = User.create(
+      email: 'test@test.com',
+      password: 'testing123',
+      username: 'test'
+    )
+
+    sub = Sub.create(
+      user: user,
+      name: 'sub name',
+      description: 'sub description'
+    )
+
+    post = Post.create(
+      title: 'post title',
+      content: 'post content',
+      user: user,
+      sub: sub
+    )
+
+    vote = Vote.create(
+      user_id: user.id,
+      post_id: post.id,
+      upvote: 0,
+      downvote: 1
+    )
+
+    post_two = Post.create(
+      title: 'post title',
+      content: 'post content',
+      user: user,
+      sub: sub
+    )
+
+    # upvoted post this time
+    vote_two = Vote.create(
+      user_id: user.id,
+      post_id: post_two.id,
+      upvote: 1,
+      downvote: 0
+    )
+
+    user_downvoted_posts = user.downvoted_posts
+    all_downvoted_posts = Post.joins(:votes).where(:votes => { upvote: 0, downvote: 1} )
+
+    expect(user_downvoted_posts).to match_array(all_downvoted_posts)
   end
 end

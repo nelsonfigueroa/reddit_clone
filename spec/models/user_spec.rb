e326073b859aedfb291d88063fa1951e4f3e30d7
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-09-17 20:25:03 -0700

    Added context for clarity

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,196 +19,198 @@ RSpec.describe User, type: :model do
     end
   end
 
-  it 'determines whether the user has voted on a post with #has_not_voted' do
-    user_one = User.create(
-      email: 'test@one.com',
-      password: 'testing123!',
-      username: 'test_one'
-    )
+  context 'when voting as a user' do
+    it 'determines whether the user has voted on a post with #has_not_voted' do
+      user_one = User.create(
+        email: 'test@one.com',
+        password: 'testing123!',
+        username: 'test_one'
+      )
 
-    user_two = User.create(
-      email: 'test@two.com',
-      password: 'testing123!',
-      username: 'test_two'
-    )
+      user_two = User.create(
+        email: 'test@two.com',
+        password: 'testing123!',
+        username: 'test_two'
+      )
 
-    sub = Sub.create(
-      user: user_two,
-      name: 'sub name',
-      description: 'sub description'
-    )
+      sub = Sub.create(
+        user: user_two,
+        name: 'sub name',
+        description: 'sub description'
+      )
 
-    post = Post.create(
-      title: 'post title',
-      content: 'post content',
-      user: user_two,
-      sub: sub
-    )
+      post = Post.create(
+        title: 'post title',
+        content: 'post content',
+        user: user_two,
+        sub: sub
+      )
 
-    vote = Vote.create(
-      user_id: user_two.id,
-      post_id: post.id,
-      upvote: 1,
-      downvote: 0
-    )
+      vote = Vote.create(
+        user_id: user_two.id,
+        post_id: post.id,
+        upvote: 1,
+        downvote: 0
+      )
 
-    expect(user_one.has_not_voted?(post)).to be true
-    expect(user_two.has_not_voted?(post)).to be false
-  end
+      expect(user_one.has_not_voted?(post)).to be true
+      expect(user_two.has_not_voted?(post)).to be false
+    end
 
-  it 'determines whether a user has upvoted a post with #upvoted_post?' do
-    user = User.create(
-      email: 'test@test.com',
-      password: 'testing123',
-      username: 'test'
-    )
+    it 'determines whether a user has upvoted a post with #upvoted_post?' do
+      user = User.create(
+        email: 'test@test.com',
+        password: 'testing123',
+        username: 'test'
+      )
 
-    sub = Sub.create(
-      user: user,
-      name: 'sub name',
-      description: 'sub description'
-    )
+      sub = Sub.create(
+        user: user,
+        name: 'sub name',
+        description: 'sub description'
+      )
 
-    post = Post.create(
-      title: 'post title',
-      content: 'post content',
-      user: user,
-      sub: sub
-    )
+      post = Post.create(
+        title: 'post title',
+        content: 'post content',
+        user: user,
+        sub: sub
+      )
 
-    vote = Vote.create(
-      user_id: user.id,
-      post_id: post.id,
-      upvote: 1,
-      downvote: 0
-    )
+      vote = Vote.create(
+        user_id: user.id,
+        post_id: post.id,
+        upvote: 1,
+        downvote: 0
+      )
 
-    expect(user.upvoted_post?(post)).to be true
-  end
+      expect(user.upvoted_post?(post)).to be true
+    end
 
-  it 'determines whether a user has downvoted a post with #downvoted_post?' do
-    user = User.create(
-      email: 'test@test.com',
-      password: 'testing123',
-      username: 'test'
-    )
+    it 'determines whether a user has downvoted a post with #downvoted_post?' do
+      user = User.create(
+        email: 'test@test.com',
+        password: 'testing123',
+        username: 'test'
+      )
 
-    sub = Sub.create(
-      user: user,
-      name: 'sub name',
-      description: 'sub description'
-    )
+      sub = Sub.create(
+        user: user,
+        name: 'sub name',
+        description: 'sub description'
+      )
 
-    post = Post.create(
-      title: 'post title',
-      content: 'post content',
-      user: user,
-      sub: sub
-    )
+      post = Post.create(
+        title: 'post title',
+        content: 'post content',
+        user: user,
+        sub: sub
+      )
 
-    vote = Vote.create(
-      user_id: user.id,
-      post_id: post.id,
-      upvote: 0,
-      downvote: 1
-    )
+      vote = Vote.create(
+        user_id: user.id,
+        post_id: post.id,
+        upvote: 0,
+        downvote: 1
+      )
 
-    expect(user.downvoted_post?(post)).to be true
-  end
+      expect(user.downvoted_post?(post)).to be true
+    end
 
-  it 'returns posts that were upvoted by user with #upvoted_posts' do
-    user = User.create(
-      email: 'test@test.com',
-      password: 'testing123',
-      username: 'test'
-    )
+    it 'returns posts that were upvoted by user with #upvoted_posts' do
+      user = User.create(
+        email: 'test@test.com',
+        password: 'testing123',
+        username: 'test'
+      )
 
-    sub = Sub.create(
-      user: user,
-      name: 'sub name',
-      description: 'sub description'
-    )
+      sub = Sub.create(
+        user: user,
+        name: 'sub name',
+        description: 'sub description'
+      )
 
-    post = Post.create(
-      title: 'post title',
-      content: 'post content',
-      user: user,
-      sub: sub
-    )
+      post = Post.create(
+        title: 'post title',
+        content: 'post content',
+        user: user,
+        sub: sub
+      )
 
-    vote = Vote.create(
-      user_id: user.id,
-      post_id: post.id,
-      upvote: 1,
-      downvote: 0
-    )
+      vote = Vote.create(
+        user_id: user.id,
+        post_id: post.id,
+        upvote: 1,
+        downvote: 0
+      )
 
-    post_two = Post.create(
-      title: 'post title',
-      content: 'post content',
-      user: user,
-      sub: sub
-    )
+      post_two = Post.create(
+        title: 'post title',
+        content: 'post content',
+        user: user,
+        sub: sub
+      )
 
-    # downvoted post this time
-    vote_two = Vote.create(
-      user_id: user.id,
-      post_id: post_two.id,
-      upvote: 0,
-      downvote: 1
-    )
+      # downvoted post this time
+      vote_two = Vote.create(
+        user_id: user.id,
+        post_id: post_two.id,
+        upvote: 0,
+        downvote: 1
+      )
 
-    user_upvoted_posts = user.upvoted_posts
-    all_upvoted_posts = Post.joins(:votes).where(votes: { upvote: 1, downvote: 0 })
+      user_upvoted_posts = user.upvoted_posts
+      all_upvoted_posts = Post.joins(:votes).where(votes: { upvote: 1, downvote: 0 })
 
-    expect(user_upvoted_posts).to match_array(all_upvoted_posts)
-  end
+      expect(user_upvoted_posts).to match_array(all_upvoted_posts)
+    end
 
-  it 'returns posts that were downvoted by a user with #downvoted_posts' do
-    user = User.create(
-      email: 'test@test.com',
-      password: 'testing123',
-      username: 'test'
-    )
+    it 'returns posts that were downvoted by a user with #downvoted_posts' do
+      user = User.create(
+        email: 'test@test.com',
+        password: 'testing123',
+        username: 'test'
+      )
 
-    sub = Sub.create(
-      user: user,
-      name: 'sub name',
-      description: 'sub description'
-    )
+      sub = Sub.create(
+        user: user,
+        name: 'sub name',
+        description: 'sub description'
+      )
 
-    post = Post.create(
-      title: 'post title',
-      content: 'post content',
-      user: user,
-      sub: sub
-    )
+      post = Post.create(
+        title: 'post title',
+        content: 'post content',
+        user: user,
+        sub: sub
+      )
 
-    vote = Vote.create(
-      user_id: user.id,
-      post_id: post.id,
-      upvote: 0,
-      downvote: 1
-    )
+      vote = Vote.create(
+        user_id: user.id,
+        post_id: post.id,
+        upvote: 0,
+        downvote: 1
+      )
 
-    post_two = Post.create(
-      title: 'post title',
-      content: 'post content',
-      user: user,
-      sub: sub
-    )
+      post_two = Post.create(
+        title: 'post title',
+        content: 'post content',
+        user: user,
+        sub: sub
+      )
 
-    # upvoted post this time
-    vote_two = Vote.create(
-      user_id: user.id,
-      post_id: post_two.id,
-      upvote: 1,
-      downvote: 0
-    )
+      # upvoted post this time
+      vote_two = Vote.create(
+        user_id: user.id,
+        post_id: post_two.id,
+        upvote: 1,
+        downvote: 0
+      )
 
-    user_downvoted_posts = user.downvoted_posts
-    all_downvoted_posts = Post.joins(:votes).where(votes: { upvote: 0, downvote: 1 })
+      user_downvoted_posts = user.downvoted_posts
+      all_downvoted_posts = Post.joins(:votes).where(votes: { upvote: 0, downvote: 1 })
 
-    expect(user_downvoted_posts).to match_array(all_downvoted_posts)
+      expect(user_downvoted_posts).to match_array(all_downvoted_posts)
+    end
   end
 end

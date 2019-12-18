e72b2152c84247df44b9b7652b0db5c13b3ee1ee
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2019-12-17 22:24:01 -0800

    renamed from sub to forum

---
+++ spec/models/forum_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Forum, type: :model do
+  let(:post) { Post.new }
+  let(:user) { User.new }
+  let(:forum) { Forum.new }
+
+  context 'creating a forum' do
+    it 'returns invalid if attributes are missing' do
+      expect(forum.valid?).to be false
+    end
+
+    it 'validates that all attributes are present' do
+      forum.name = Faker::String.random
+      forum.description = Faker::String.random
+      forum.user = user
+
+      expect(forum.valid?).to be true
+    end
+
+    it 'validates that forum name is unique' do
+      forum.name = Faker::String.random
+      forum.description = Faker::String.random
+      forum.user = user
+      forum.save!
+
+      forum_two = forum.dup
+      forum_two.user = user
+
+      # curly braces needed in this case
+      expect { forum_two.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+end

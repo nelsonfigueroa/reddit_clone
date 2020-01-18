b87d72b3f324cfec718ceeff45e4be0e2a6f0909
Nelson Figueroa <nelsonfigueroa07@gmail.com>
2020-01-17 21:45:13 -0800

    removed more comments

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -21,21 +21,6 @@ RSpec.describe Post, type: :model do
   let(:user) { User.new }
   let(:forum) { Forum.new }
 
-  # context 'creating a post' do
-  #   it 'returns invalid if attributes are missing' do
-  #     expect(post.valid?).to be false
-  #   end
-
-  #   it 'validates that all attributes are present' do
-  #     post.title = Faker::String.random
-  #     post.content = Faker::String.random
-  #     post.user = user
-  #     post.forum = forum
-  #     post.save!
-  #     expect(post.valid?).to be true
-  #   end
-  # end
-
   context 'when calling #net_votes' do
     it 'returns an integer' do
       expect(post.net_votes).to be_an(Integer)
